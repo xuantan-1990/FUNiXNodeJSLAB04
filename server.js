b81aca479745be356c24a620e351e6b0a6efae4b
Ho Xuan Tan <tanhxFX15168@funix.edu.vn>
2022-08-25 14:39:24 +0700

    LAB01_7

--- server.js
+++ server.js
@@ -1,40 +1,14 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write("<html>");
-        res.write("<head><title>Enter Message</title></head>");
-        res.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>");
-        res.write("</html>");
-        return res.end();
-    }
+const routes = require('./routes')
 
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-fs.writeFileSync("message.txt", message);
-        })
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First NodeJS Page</title></head>');
-    res.write("<body><h1>Hello from my Node.js server!</h1></body>");
-    res.write("</html>");
-    res.end();
-});
+//trong thư mục nhận chỉ có 1 hàm
+const server = http.createServer(routes);
+
+//trong thư mục nhận chỉ có nhiều hàm
+/*
+const server = http.createServer(routes.handler);
+const server = http.createServer(routes.someText);
+*/
 
 server.listen(3000)
