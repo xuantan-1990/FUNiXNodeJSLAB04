2bc4bc90a70a4411d04f39c8806765ba03295054
Ho Xuan Tan <tanhxFX15168@funix.edu.vn>
2022-08-25 12:51:11 +0700

    LAB01_6

--- server.js
+++ server.js
@@ -14,7 +14,17 @@ const server = http.createServer((req, res) => {
     }
 
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+fs.writeFileSync("message.txt", message);
+        })
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
