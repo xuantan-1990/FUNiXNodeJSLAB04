2a07b08e6a80fc77019dcff03ebf1c9c562c2d53
Ho Xuan Tan <tanhxFX15168@funix.edu.vn>
2022-09-06 16:24:26 +0700

    LAB02.12

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 /*
-  - Lab 2.11: Cung cấp các trang HTML
-Sử dụng path để lấy đường dẫn file html và trả về client (thay về trả trực tiếp từ string)
+  - Lab 2.12: Trả lại trang 404
+Tương tự như trên áp dụng cho trang 404 ở app.js.
 */
 
 const express = require("express");
 const appServer = express();
 const routerAdmin = require("./routers/admin");
 const routerShop = require("./routers/shop");
+const path = require("path");
 
 appServer.use(express.urlencoded({ extended: false }));
 
 appServer.use("/admin", routerAdmin);
 appServer.use(routerShop);
 appServer.use((req, res) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 appServer.listen(3000);
