b64e7e01c3e8203a71c826da8c9e60a54ae40a25
Ho Xuan Tan <tanhxFX15168@funix.edu.vn>
2022-09-06 20:45:33 +0700

    LAB03.1

--- routers/admin.js
+++ routers/admin.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const rootDir = require('../util/path');
+const products = [];
 
 router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
-router.post("/product", (req, res, next) => {
-  console.log(req.body);
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 /*
-  - Lab 2.14: Cung cấp tệp tĩnh
- Dẫn link tương đối các file css bằng cách đưa vào folder public/css.
+  - Lab 3.1: Chia sẻ dữ liệu giữa các Requests, User
+Tạo object products và chia sẻ giữa trang admin, shop.
 */
 
 const express = require("express");
 const appServer = express();
-const routerAdmin = require("./routers/admin");
+const adminData = require("./routers/admin");
 const routerShop = require("./routers/shop");
 const path = require("path");
 
 appServer.use(express.urlencoded({ extended: false }));
 appServer.use(express.static(path.join(__dirname, "public")));
 
-appServer.use("/admin", routerAdmin);
+appServer.use("/admin", adminData.routes);
 appServer.use(routerShop);
 appServer.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
