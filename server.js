9dbb83d2f21ef5cfc3584a7e25d54fa36416c449
Ho Xuan Tan <tanhxFX15168@funix.edu.vn>
2022-09-06 13:42:21 +0700

    LAB02.4

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 /*
-  - Lab 2.3: Cách thức hoạt động của Middleware
-Ở middleware thứ 2 trả về một đoạn code HTML với nội dung: Hello from Express.js
+  - Lab 2.4: Xử lý các Route khác nhau
+Tạo 3 middleware:
+    + 1 cái luôn chạy.
+    + 1 cái ứng với http://localhost:3000/add-product trả về một trang HTML với nội dung: "The Add product page".
+    + 1 cái ứng với http://localhost:3000 trả về một trang HTML với nội dung: "Hello from Express.js".
 */
 
 const http = require("http");
 const express = require("express");
 const appServer = express();
 
-appServer.use((req, res, next) => {
-  console.log("First Middleware");
+appServer.use("/", (req, res, next) => {
+  console.log("This always run");
   next();
 });
 
-appServer.use((req, res, next) => {
-  console.log("Second Middleware");
+appServer.use("/add-product", (req, res, next) => {
+  console.log("Add product Middleware");
+  res.send("<h1>The Add product page</h1>");
+  next();
+});
+
+appServer.use("/", (req, res, next) => {
+  console.log("Third Middleware");
   res.send("<h1>Hello from Express.js</h1>");
-  console.log("Hello from Express.js");
 });
 
 const server = http.createServer(appServer);
