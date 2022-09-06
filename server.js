a34eed6a56a890bd55d7407c8a2e2bb2d54a5042
Ho Xuan Tan <tanhxFX15168@funix.edu.vn>
2022-09-06 14:49:15 +0700

    LAB02.5&02.6

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 /*
-  - Lab 2.4: Xử lý các Route khác nhau
-Tạo 3 middleware:
-    + 1 cái luôn chạy.
-    + 1 cái ứng với http://localhost:3000/add-product trả về một trang HTML với nội dung: "The Add product page".
-    + 1 cái ứng với http://localhost:3000 trả về một trang HTML với nội dung: "Hello from Express.js".
+  - Lab 2.5: Phân tích cú pháp Requests đến
+Ở trang http://localhost:3000/add-product trả về một trang HTML với nội dung là 1 form dùng để post sản phẩm (bao gồm thông tin về title) tới http://localhost:3000/product.
+Tạo middleware ứng với http://localhost:3000/product nhận post request đến và log ra console req.body
+
+  - Lab 2.6: Giới hạn việc thực thi Middleware đối với Post Requests
+Thay đổi middleware ứng với http://localhost:3000/product để chỉ nhận post request
 */
 
-const http = require("http");
 const express = require("express");
 const appServer = express();
 
-appServer.use("/", (req, res, next) => {
-  console.log("This always run");
-  next();
-});
+appServer.use(express.urlencoded({ extended: false }));
 
 appServer.use("/add-product", (req, res, next) => {
-  console.log("Add product Middleware");
-  res.send("<h1>The Add product page</h1>");
-  next();
+  console.log("First Middleware");
+  res.send(
+    "<form action='/product' method= 'POST'><input type='text' name='title'><button type='submit'>Send</button></form>"
+  );
+});
+
+//Lab 2.5: sử dụng appServer.use
+appServer.post("/product", (req, res, next) => {
+  console.log("Second Middleware");
+  console.log(req.body);
+  res.redirect("/");
 });
 
 appServer.use("/", (req, res, next) => {
@@ -26,6 +31,4 @@ appServer.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express.js</h1>");
 });
 
-const server = http.createServer(appServer);
-
-server.listen(3000);
+appServer.listen(3000);
